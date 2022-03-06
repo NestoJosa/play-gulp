bb666dadf40d26c94b7aa1543868a1e7a29e03c0
Ernesto Hinojosa <59732463+NestoJosa@users.noreply.github.com>
2022-03-06 11:11:24 +0100

    Setup copy function

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,19 @@
-import gulp from 'gulp';
+import { src, dest } from 'gulp';
 import del from 'del';
  
- 
-/*
- * For small tasks you can export arrow functions
- */
+// Delete '/dist'
 export const clean = () => del([ 'dist' ]);
- 
+
+// Copy source files into '/dist'
+export const copy = () => {
+    return src([
+        // Copy everything from src,
+        'src/**/*',
+        // except for certain dirs,
+        '!src/{js,scss}', 
+    ])
+    // into a specified directory
+    .pipe(dest('dist'));
+}
+
+export default copy;
